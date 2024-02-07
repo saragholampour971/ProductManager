bcb666aea67989f83729fb78bf5c2b8f05778e3b
sara <saragholampour971@yahoo.com>
2024-02-07 11:38:34 +0330

    init

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using Report;
+
+namespace ProductManager;
+
+public interface IProduct
+{
+    string Label { get; set; }
+    int Count { get; set; }
+}
+
+public class ProductItem : IProduct
+{
+    public string Label { get; set; }
+    public int Count { get; set; }
+
+    public ProductItem(string label, int count)
+    {
+        this.Label = label;
+        this.Count = count;
+    }
+}
+
+
+public class Product : IReport<ProductItem>
+{
+    public string Label { get; } = "Products";
+
+    public List<ProductItem> Items { get;  }
+
+    public List<ISubMenu> SubMenus { get; }
+
+    private string GetCount ()
+    {
+        return Items.Count.ToString();
+
+    }
+
+    public Product()
+    {
+      this.Items=new List<ProductItem>()
+        {
+            new ProductItem("pen", 100),
+            new ProductItem("jacket", 50),
+            new ProductItem("umbrella", 658),
+        };
+
+      this.SubMenus=new List<ISubMenu>()
+        {
+            new SubMenu("get Count", GetCount)
+        };
+    }
+
+}
